2159745f05260db838d73c2c499c1867bdf53a12
Lukasas <LukasChabek@seznam.cz>
2010-08-22 19:29:06 +0200

    no commit

---
+++ Form1.vb
@@ -0,0 +1,32 @@
+﻿Imports System.Net.Sockets
+Imports System.Text
+Public Class Form1
+    Dim clientSocket As New System.Net.Sockets.TcpClient()
+    Dim serverStream As NetworkStream
+
+    Private Sub Button1_Click(ByVal sender As System.Object, _
+        ByVal e As System.EventArgs) Handles Button1.Click
+        Dim serverStream As NetworkStream = clientSocket.GetStream()
+        Dim buffSize As Integer
+        Dim outStream As Byte() = System.Text.Encoding.ASCII.GetBytes(TextBox1.Text + "$")
+
+        serverStream.Write(outStream, 0, outStream.Length)
+        serverStream.Flush()
+
+        Dim inStream(10024) As Byte
+        buffSize = clientSocket.ReceiveBufferSize
+        serverStream.Read(inStream, 0, buffSize)
+        Dim returndata As String = System.Text.Encoding.ASCII.GetString(inStream)
+        msg("Zprava od serveru : " + returndata)
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        msg("Client Started")
+        clientSocket.Connect("127.0.0.1", 8888)
+        Label1.Text = "Client - Server Connected ..."
+    End Sub
+    'updava textu
+    Sub msg(ByVal mesg As String)
+        TextBox1.Text = TextBox1.Text + Environment.NewLine + " >> " + mesg
+    End Sub
+End Class
---
+++ Module1.vb
@@ -0,0 +1,80 @@
+﻿Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Module Module1
+    Sub Main()
+        Dim serverSocket As New TcpListener(8888)
+        Dim clientSocket As TcpClient
+        Dim counter As Integer
+        ' spuštení serveru
+        serverSocket.Start()
+        msg("Server Started")
+        ' nastavení počítání clientů na 0
+        counter = 0
+        While (True)
+            counter += 1
+            clientSocket = serverSocket.AcceptTcpClient()
+            msg("Client Number:" + Convert.ToString(counter) + " connected!")
+            Dim client As New handleClinet
+            client.startClient(clientSocket, Convert.ToString(counter))
+        End While
+
+        clientSocket.Close()
+        serverSocket.Stop()
+        msg("Server Konci")
+        Console.ReadLine()
+    End Sub
+    ' uprava textu
+    Sub msg(ByVal mesg As String)
+        mesg.Trim()
+        Console.WriteLine(" >> " + mesg)
+    End Sub
+
+    Public Class handleClinet
+        Dim clientSocket As TcpClient
+        Dim clNum As String
+
+        Public Sub startClient(ByVal ClientsSocket As TcpClient, ByVal clineNumber As String)
+            Me.clientSocket = ClientsSocket
+            Me.clNum = clineNumber
+            Dim ctThread As Thread = New Thread(AddressOf doChat)
+            ctThread.Start()
+        End Sub
+
+        Private Sub doChat()
+            Dim requestCount As Integer
+            Dim bytesFrom(10024) As Byte
+            Dim dataFromClient As String
+            Dim sendBytes As [Byte]()
+            Dim serverResponse As String
+            Dim rCount As String
+
+            requestCount = 0
+
+            While (True)
+                Try
+                    requestCount = requestCount + 1
+                    Dim networkStream As NetworkStream = clientSocket.GetStream()
+                    networkStream.Read(bytesFrom, 0, CInt(clientSocket.ReceiveBufferSize))
+
+                    dataFromClient = System.Text.Encoding.ASCII.GetString(bytesFrom)
+                    dataFromClient = dataFromClient.Substring(0, dataFromClient.IndexOf("$"))
+                    msg("Od clienta cislo - " + clNum + " Text: " + dataFromClient)
+
+                    rCount = Convert.ToString(requestCount)
+                    serverResponse = "Server clientu c.(" + clNum + ") " + rCount
+                    sendBytes = Encoding.ASCII.GetBytes(serverResponse)
+
+                    networkStream.Write(sendBytes, 0, sendBytes.Length)
+                    networkStream.Flush()
+                    msg(serverResponse)
+
+                Catch ex As Exception
+                    MsgBox(ex.ToString)
+                End Try
+
+            End While
+
+        End Sub
+    End Class
+End Module
